8ade7f03663d894742a08096179152d7311786f8
Junghyun Min <jmin10@jhu.edu>
2025-06-27 18:25:09 -0400

    [Chore] Refactor code
    * Refactor code to allow modern framework support
    * Update pipeline to take Wikipedia data as training set

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name="pr"
+#SBATCH --output="%x.o%j"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu
+#SBATCH --cpus-per-task=4
+#SBATCH --time=48:00:00
+#SBATCH --mem=0
+#SBATCH --mail-user=jm3743@georgetown.edu
+#SBATCH --mail-type=END,FAIL
+
+source env.sh
+python prep-pr-data.py
+python main.py

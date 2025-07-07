5c9d81c07d7d0f7375bc08a47f58fc480870d2a1
Rex Fang <h39fang@uwaterloo.ca>
2025-07-07 16:50:01 -0400

    more time for main to run

---
+++ jobs/train.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --account=def-annielee
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus=1
+#SBATCH --mem=32G
+#SBATCH --time=6:0:0
+module load python gcc arrow
+source .env/bin/activate
+python main.py
+deactivate

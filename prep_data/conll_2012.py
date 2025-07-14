47ffd10dd8959f7e267146ab7939c0044438dd04
Rex Fang <i@rexylic.com>
2025-07-14 18:12:19 -0400

    CoNLL 2012 data prep testing

--- prep_data/conll_2012.py
+++ prep_data/conll_2012.py
@@ -1,28 +1,35 @@
 from datasets import load_dataset
 import json
+from pprint import pprint
 
 class CoNLL2012:
 
     def __init__(self, split='train'):
-        self.data = load_dataset('ontonotes/conll2012_ontonotesv5', 'english_v4', split=split)
+        self.data = load_dataset(
+            'ontonotes/conll2012_ontonotesv5',
+            'english_v4',
+            split=split,
+            # streaming=True, # doesn't work since have to index 'sentences'
+        )
 
-    def tags(self, feature='srl_frames'):
-        return self.data.features[feature].feature.names
-
-    def label(self, i, feature='srl_frames'):
-        return self.tags(feature)[i]
+    def features(self):
+        return self.data.features
 
     def dump_json(self, path='conll_2012.jsonl'):
         count = 0
         with open(path, 'w', encoding='utf-8') as file:
-            for row in self.data:
-                source = ' '.join(row['tokens'])
-                target = ' '.join([self.label(i) for i in row['pos_tags']])
-                json.dump({"source": source, "target": target}, file, ensure_ascii=False)
-                file.write("\n")
-                count += 1
-        print(f"[INFO] Wrote {count} rows to {path}")
+            for paragraph in self.data['sentences']:
+                for sentence in paragraph:
+                    source = ' '.join(sentence['words'])
+                    target = ''
+                    for srl_frame in sentence['srl_frames']:
+                        verb = srl_frame['verb']
+                        frames = srl_frame['frames']
+                        target += f'{verb}:({" ".join(frames)}) '
+                    file.write(json.dumps({"source": source, "target": target.rstrip()}) + '\n')
+                    count += 1
+        print(f"[INFO] Wrote {count} examples to {path}")
 
 if __name__ == '__main__':
     ds = CoNLL2012()
-    print(ds.data.features)
+    ds.dump_json()

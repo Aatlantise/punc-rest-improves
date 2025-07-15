4be3ba239c330386e456a5665ea22bd7bf4eb445
Rex Fang <h39fang@uwaterloo.ca>
2025-07-15 11:06:53 -0400

    small fixes

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --account=def-annielee
 #SBATCH --cpus-per-task=1
-#SBATCH --gpus-per-task=1
+#SBATCH --gpus=1
 #SBATCH --job-name="srl-training-attempt"
 #SBATCH --mem=16G
 #SBATCH --output="slurm-logs/%x-%j"
 #SBATCH --time=10:00
 
 source env.sh
-python main.py
+python train.py

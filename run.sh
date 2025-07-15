6b8b3473032cd724f7639e64c5ed2b6d3f5ee2d6
Rex Fang <h39fang@uwaterloo.ca>
2025-07-15 11:03:51 -0400

    update scripts

--- run.sh
+++ run.sh
@@ -2,14 +2,11 @@
 
 #SBATCH --account=def-annielee
 #SBATCH --cpus-per-task=1
-#SBATCH --gpus-per-task=0
-#SBATCH --job-name="testing-conll-2012-data-prep"
-#SBATCH --mail-user=h39fang@uwaterloo.ca
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mem=8G
-#SBATCH --output="%x.o%j"
-#SBATCH --time=5:00
+#SBATCH --gpus-per-task=1
+#SBATCH --job-name="srl-training-attempt"
+#SBATCH --mem=16G
+#SBATCH --output="slurm-logs/%x-%j"
+#SBATCH --time=10:00
 
 source env.sh
-pip install -r requirements.txt
-python prep_data/conll_2012.py --trust_remote_code
+python main.py

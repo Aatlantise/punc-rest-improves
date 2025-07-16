d52cfbc53a6adc526d0394b62fcee4763fbcdd1b
Rex Fang <h39fang@uwaterloo.ca>
2025-07-16 14:59:03 -0400

    updated job scripts

--- slurm/jobs/env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-module load arrow cuda gcc python
-source .env/bin/activate # needs to be run with source, not bash
-pip install -r requirements.txt # make sure all requirements are satisfied 
-export TOKENIZERS_PARALLELISM=true
--- slurm/jobs/run.sh
+++ slurm/jobs/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-#SBATCH --account=def-annielee
-#SBATCH --cpus-per-task=1
-#COMMENT --gpus=1
-#SBATCH --job-name='data-prep'
-#SBATCH --mail-user='rex.fang@icloud.com'
-#SBATCH --mem=16G
-#SBATCH --output="slurm/logs/%x-%j"
-#SBATCH --time=1:0:0
+#SBATCH -c 4
+#SBATCH -G 1
+#SBATCH -t 12:0:0
 
-source env.sh
-python prep_data/wiki.py
-python prep_data/conll_2012.py
+#SBATCH --job-name='srl-train'
+#SBATCH --mail-user='rex.fang@icloud.com'
+#SBATCH --mail-type=ALL
+#SBATCH --mem=32G
+#SBATCH --output="slurm/logs/%x-%j"
+
+module load arrow cuda gcc python
+source .env/bin/activate # needs to be run with source, not bash
+pip install -r requirements.txt # make sure all requirements are satisfied 
+export TOKENIZERS_PARALLELISM=true
+python train.py

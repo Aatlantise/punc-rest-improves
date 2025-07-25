e125b9ebbbf177e855a2f5d8a2a1a1033838775d
Rex Fang <i@rexylic.com>
2025-07-25 07:40:38 -0400

    epoch parsing update

--- train.py
+++ train.py
@@ -1,16 +1,15 @@
-## Rex's refactor of main.py
-
 import lightning
 import logging
 import numpy as np
 import random
+import re
 import sys
 import torch
 
 from argparse import ArgumentParser
 from data.modules import TrainData
 from datetime import datetime
-from lightning import Trainer
+from lightning import Callback, Trainer
 from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor
 from lightning.pytorch.loggers import TensorBoardLogger
 from models.t5 import PRT5
@@ -33,18 +32,34 @@ def set_seed(seed: int):
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
+        
+class IndividualCheckpoints(Callback):
+    def __init__(self, epochs_to_save_at: set[int], save_dir: str = 'outputs/checkpoints', name: str = 'mlm'):
+        super().__init__()
+        self.epochs_to_save_at = epochs_to_save_at
+        self.save_dir = save_dir
+        self.name = name
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        epoch = trainer.current_epoch
+        if epoch in self.epochs_to_save_at:
+            timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+            ckpt_path = join_paths(self.save_dir, '%s-%s-epoch%s.ckpt' % (self.name, timestamp, epoch))
+            trainer.save_checkpoint(ckpt_path)
+            logger.info('Saved checkpoint at %s' % ckpt_path)
 
 def run(
     ckpt_filename: str,
     data_path: str,
     save_last_epoch: bool,
+    min_epochs: int,
+    max_epochs: int,
     accelerator: str = 'gpu',
     adam_epsilon: float = 1e-8,
     devices: int = 1,
     eval_batch_size: int = 32,
     learning_rate: float = 3e-4,
     log_every_n_steps: int = 10,
-    max_epochs = 3,
     max_seq_length: int = 512,
     model_name_or_path = 'google-t5/t5-base',
     monitor_metric: str = 'val_loss',
@@ -83,6 +98,7 @@ def run(
     
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
     trainer = Trainer(
+        min_epochs = min_epochs,
         max_epochs = max_epochs,
         logger = TensorBoardLogger(save_dir = join_paths(output_dir, 'logs'), name = ckpt_filename),
         callbacks = [
@@ -94,7 +110,8 @@ def run(
                 monitor = monitor_metric,
                 mode = 'min',
             ),
-            LearningRateMonitor(logging_interval = 'step')
+            LearningRateMonitor(logging_interval = 'step'),
+            IndividualCheckpoints(epochs_to_save_at = {0, 9}), # Save 1st and 10th epochs
         ],
         precision = precision,
         accelerator = accelerator,
@@ -135,6 +152,15 @@ if __name__ == '__main__':
         help = 'The training task to perform.',
     )
     parser.add_argument(
+        '-n', '--ckpt-name',
+        type = str, required = True,
+        help = """
+                Name the checkpoints that will be saved for this training.
+                Timestamps, val_loss for each epoch might be appended.
+                File extension will be appended.
+                """
+    )
+    parser.add_argument(
         '-d', '--dataset-jsonl',
         type = str,
         help = """
@@ -143,12 +169,13 @@ if __name__ == '__main__':
             """,
     )
     parser.add_argument(
-        '-n', '--ckpt-name',
-        type = str,
+        '-e', '--epochs',
+        type = str, default = '1-3',
         help = """
-            Name the checkpoints that will be saved for this training.
-            Timestamps, val_loss for each epoch might be appended.
-            File extension will be appended.
+            Number of epochs to run.
+            
+            For example, `3` means to run for exactly 3 epochs.
+            `2-5` means to run for at least 2 epochs but at most 5 epochs.
             """
     )
     parser.add_argument(
@@ -167,20 +194,33 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
     
-    match args.task:
-        case 'pr':
-            run(
-                data_path = 'outputs/datasets/wiki-20231101.en-pr.jsonl',
-                resume_ckpt = args.resume_ckpt,
-                ckpt_filename = args.ckpt_name or 'pr',
-                save_last_epoch = args.save_last_epoch,
-            )
-        case 'srl':
-            run(
-                data_path = 'outputs/datasets/conll-2012-srl-512t.jsonl',
-                resume_ckpt = args.resume_ckpt or 'outputs/checkpoints/pr.20250717-161054.epoch=1-val_loss=0.1053.ckpt',
-                ckpt_filename = args.ckpt_name or 'pr-srl-512t',
-                save_last_epoch = args.save_last_epoch,
-            )
-        case _:
-            raise Exception('Task %s has not been implemented. Aborting...' % args.task)
+    default_data_paths = {
+        'pr': 'outputs/datasets/wiki-20231101.en-pr.jsonl',
+        'mlm': 'outputs/datasets/wiki-20231101.en-mlm.jsonl',
+        'srl': 'outputs/datasets/conll-2012-srl-512t.jsonl'
+    }
+    default_resume_ckpts = {
+        'srl': 'outputs/checkpoints/pr.20250717-161054.epoch=1-val_loss=0.1053.ckpt'
+    }
+    
+    if args.task not in default_data_paths.keys():
+        raise Exception('Task %s has not been implemented. Aborting...' % args.task)
+    
+    min_epochs, max_epochs = 0, 0
+    if re.fullmatch(r'\d+-\d+', args.epochs):
+        s = args.epochs.split('-')
+        min_epochs, max_epochs = int(s[0]), int(s[1])
+    elif re.fullmatch(r'\d+', args.epochs):
+        min_epochs = int(args.epochs)
+        max_epochs = min_epochs
+    else:
+        raise SyntaxError(f'Option -e/--epoch received invalid argument "{args.epochs}"')
+    
+    run(
+        data_path = args.dataset_jsonl or default_data_paths[args.task],
+        resume_ckpt = args.resume_ckpt or default_resume_ckpts.get(args.task),
+        ckpt_filename = args.ckpt_name,
+        save_last_epoch = args.save_last_epoch,
+        min_epochs = min_epochs,
+        max_epochs = max_epochs,
+    )

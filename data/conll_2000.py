8f241b26b2cb9d4907408d360d0a287c02999c5e
Rex Fang <i@rexylic.com>
2025-08-07 11:29:58 -0400

    conll00 added chunking

--- data/conll_2000.py
+++ data/conll_2000.py
@@ -16,18 +16,43 @@ class CoNLL2000(PrepData):
     def id_to_pos_tag(self, i: int) -> str:
         return self.data['train'].features['pos_tags'].feature.names[i]
     
+    def id_to_chunk_tag(self, i: int) -> str:
+        return self.data['train'].features['chunk_tags'].feature.names[i]
+    
     def src_tgt_pairs(self, task: str):
-        if task not in ['pos']:
+        if task not in ['pos', 'chunking']:
             raise NotImplementedError(f'Task {task} not implemented. ')
         for _, split in self.data.items():
             for example in split:
-                tokens, tags = example['tokens'], example['pos_tags']
+                tokens = example['tokens']
                 source = ' '.join(tokens)
-                target = ' '.join(map(self.id_to_pos_tag, tags))
-                yield source, target
+                match task:
+                    case 'pos':
+                        tags = example['pos_tags']
+                        target = ' '.join(map(self.id_to_pos_tag, tags))
+                        yield source, target
+                    case 'chunking':
+                        tags = map(self.id_to_chunk_tag, example['chunk_tags'])
+                        target = []
+                        curr = ""
+                        for token, tag in zip(tokens, tags):
+                            if tag.startswith("B-"):
+                                if curr:
+                                    target.append(curr)
+                                curr = f"({token}:{tag[2:]})"
+                            elif tag.startswith("I-"):
+                                curr = curr[:-len(tag[2:]) - 2] + f" {token}:{tag[2:]})"
+                            else:
+                                if curr:
+                                    target.append(curr)
+                                curr = ""
+                        if curr:
+                            target.append(curr)
+                        target_string = " ".join(target) if target else "O"  # O if no predictions
+                        yield source, target_string
 
 if __name__ == '__main__':
     o = CoNLL2000()
-    print(o.data['train'])
     o.to_json('pos', 'conll-2000-pos')
+    o.to_json('chunking', 'conll-2000-chunking')
     

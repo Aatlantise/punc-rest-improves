68655cb6e7c2cb58e8e0a0ee944bc7318ccb02d2
Michael-Z-J <michael.zh.nz@gmail.com>
2025-09-07 12:56:16 -0400

    qqp dataset

---
+++ data/qqp.py
@@ -0,0 +1,32 @@
+from utils import logger
+from datasets import load_dataset
+import json
+
+# call: python -m data.qqp
+logger = logger()
+
+# quora question pairs, paraphrase if two questions are equal
+def generate_qqp():
+    qqp_train = load_dataset("glue", "qqp", split="train")
+    qqp_dev = load_dataset("glue", "qqp", split="validation")
+    qqp_test = load_dataset("glue", "qqp", split="test")
+    splits = ["qqp_train", "qqp_dev", "qqp_test"]
+    myMap = {"qqp_train": qqp_train, "qqp_dev": qqp_dev, "qqp_test": qqp_test}
+    for outputPath in splits:
+        excerpt_count = 0
+        print("preparing", outputPath)
+        with open(f"outputs/datasets/{outputPath}.jsonl", 'w', encoding='utf-8') as fout:
+            for example in myMap[outputPath]:
+                # Note that the test set does not have labels and it's all -1
+                text1 = example["question1"]
+                text2 = example["question2"]
+                label = example["label"]
+                json.dump({'source': "(" + text1 + ") (" + text2 + ")", 'target': str(label)}, fout, ensure_ascii=False)
+                fout.write('\n')
+
+                excerpt_count += 1
+        print(f"prepared from {excerpt_count} excerpts for {outputPath}")
+
+if __name__ == "__main__":
+    print("qqp dataset \n")
+    generate_qqp()

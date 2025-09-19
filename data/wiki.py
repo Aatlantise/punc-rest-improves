eb70f917b62ee1e6c2ecdbbb219512db69508b54
Rex Fang <i@rexylic.com>
2025-09-19 09:28:38 +0800

    New cleaning procedure

--- data/wiki.py
+++ data/wiki.py
@@ -1,14 +1,121 @@
+import nltk
 import random
 import re
 
 from data.modules import PrepData
 from nltk.tokenize import sent_tokenize, word_tokenize
-from utils import progress
+from utils import progress, logger
+
+logger = logger(__name__)
+
 
 # Constants
 PUNCTUATION_TO_REMOVE = {',', '.', '!', '?', '"', '’', '“', '”', "'"}
 MAX_WORDS = 150
 MAX_EXCERPTS = 450000
+
+
+def bookending_chars(s: str) -> tuple[str, str]:
+    """Get first and last non-space characters"""
+    stripped = s.strip()
+    if len(stripped) < 1:
+        return '', ''
+    return stripped[0], stripped[-1]
+
+def is_like_citation(s: str) -> bool:
+    return re.match(r'\s[A-Za-z\d.\u2013]+', s) is not None # u2013 is the en-dash
+
+def is_like_sentence(s: str) -> bool:
+    """Long-ish, starts with a letter and has an ending punctuation"""
+    if len(s) <= 30:
+        return False
+    first, last = bookending_chars(s)
+    return first.isalpha() and last in set(',.;:!?') and not is_like_citation(s)
+
+def remove_empty_brackets(s: str) -> str:
+    """Removes brackets that have nothing between them
+    
+    These may originate from parenthesized hyperlinks, the links and their text labels were lost in processing
+    """
+    s = re.sub(r'\(\W*?\)', '', s)
+    s = re.sub(r'\[\W*?\]', '', s)
+    return s
+
+def remove_multiple_spaces(s: str) -> str:
+    s = re.sub(r' +', ' ', s)
+    return s
+
+def remove_repeating_punctuations(s: str) -> str:
+    s = re.sub(r'([,;\.] ?){2,}', '\1', s)
+    return s
+
+def remove_brackets_with_missing_content(s: str) -> str:
+    """Removes brackets that don't neighbour a non-space inside
+    
+    These might be from missing widgets, like phonetic descriptions and links to recordings
+    """
+    s = re.sub(r'\(\W+.+?\)', '', s)
+    s = re.sub(r'\(.+?\W+\)', '', s)
+    s = re.sub(r'\[\W+.+?\]', '', s)
+    s = re.sub(r'\[.+?\W+\]', '', s)
+    return s
+
+def normalize_spaces(s: str) -> str:
+    """Turn non-space separating characters into regular spaces, might be more"""
+    s = re.sub(r'\x01', ' ', s)
+    s = re.sub(r'\u00a0', ' ', s)
+    return s
+
+def pre_process(s: str) -> str:
+    """This is done before list-collation"""
+    s = remove_empty_brackets(s)
+    s = remove_repeating_punctuations(s)
+    s = remove_brackets_with_missing_content(s)
+    return s
+
+def post_process(s: str) -> str:
+    """This is done after list-collation"""
+    s = remove_empty_brackets(s)
+    s = remove_repeating_punctuations(s)
+    s = normalize_spaces(s)
+    s = remove_multiple_spaces(s)
+    return s
+
+def cleaned(lines: list[str]) -> list[str]:
+    """Cleans wikipedia text"""
+    counter = 0
+    picked_lines = []
+    for long_lines in progress(lines, 'Picking Lines'):
+        for l in long_lines.split('\n'):
+            counter += 1
+            if re.match(r'\s[,.;]\s', l):
+                continue  # bad sentence, missing words
+            elif is_like_sentence(l):
+                picked_lines.append(l)
+    logger.debug('Picked %d lines out of %d' % (len(lines), counter))
+    
+    counter = 0
+    last_sentence = ''
+    cleaned_lines = []
+    for l in progress(picked_lines, 'Cleaning Lines'):
+        l = pre_process(l)
+        first, _ = bookending_chars(l)
+        if first == '':
+            continue
+        elif last_sentence == '':
+            last_sentence = l
+        elif first.islower():
+            last_sentence += ' ' + l.lstrip(' \t')
+        elif l[-1] == ':' or ' . ' in l or ' : ' in l:  # incomplete list or citation-like
+            continue
+        else:
+            final = post_process(last_sentence).strip()
+            cleaned_lines.append(final + '\n')
+            counter += 1
+            last_sentence = l
+    logger.debug('Wrote %s lines' % counter)
+    
+    return cleaned_lines
 
 
 def normalize_text(text):
@@ -62,25 +169,35 @@ def chunk_sentences(sentences, max_words = MAX_WORDS) -> list[str]:
 
 
 class Wiki2023(PrepData):
-    """English Wikipedia prepped for punctuation restoration"""
+    """English Wikipedia"""
 
     def __init__(self):
         super().__init__(
             path = 'wikimedia/wikipedia',
             name = '20231101.en',
         )
+    
+    def rations(self, size: int = 10000):
+        """Yields `size` lines from the source each time"""
+        ration = []
+        for _, split in self.data.items():
+            for article in progress(split, 'Wikipedia content'):
+                text = article.get('text', '')
+                if not text or len(text) < 200:
+                    continue
+                for line in text.split('\n'):
+                    if len(ration) >= size:
+                        yield cleaned(ration)
+                        ration.clear()
+                    ration.append(line)
+        return
 
     def src_tgt_pairs(self, task):
         excerpt_count = 0
-        for _, split in self.data.items():
-            for article in progress(split, 'Wiki dataprep for ' + task):
-                text = article.get("text", "")
-                if not text or len(text) < 200:
-                    continue
-
-                text = remove_reference_tags(text)
-                text = sent_tokenize(text)
-                for chunk in chunk_sentences(text, max_words = MAX_WORDS):
+        for ration in self.rations():
+            for line in ration:
+                line = sent_tokenize(line)
+                for chunk in chunk_sentences(line, max_words = MAX_WORDS):
                     target = chunk.strip()
 
                     if task == 'pr':
@@ -93,35 +210,13 @@ class Wiki2023(PrepData):
                     excerpt_count += 1
                     if excerpt_count >= MAX_EXCERPTS:
                         return
-    
-    def save_raw(self, path: str, max_lines: int = 500):
-        counter = 0
-        with open(path, 'w') as f:
-            for _, split in self.data.items():
-                for article in progress(split, 'Wikipedia'):
-                    text = article.get("text", "")
-                    if not text or len(text) < 200:
-                        continue
-                        
-                    f.write(text)
-                    f.write('\n')
-
-                    # text = sent_tokenize(text)
-                    # for chunk in chunk_sentences(text, max_words = MAX_WORDS):
-                    #     f.write(chunk.strip())
-                    #     f.write('\n')
-                
-                    counter += 1
-                    if counter >= max_lines:
-                        return
-    
 
 
 if __name__ == "__main__":
-    # nltk.download('punkt')
-    # nltk.download('punkt_tab')
-    # random.seed(42)
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
+    random.seed(42)
     ds = Wiki2023()
-    ds.save_raw('top-500-article.txt')
-    # ds.to_json('pr', 'wiki-20231101.en-pr')
-    # ds.to_json('mlm', 'wiki-20231101.en-mlm')
+    ds.to_json('pr', 'wiki-20231101.en-pr')
+    ds.to_json('mlm', 'wiki-20231101.en-mlm')
+    
